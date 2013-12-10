3250edbbfff8a5251fba54df6cc255c476a58c1b
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2013-12-10 17:59:59 -0300

    Deharcoded the summary template

--- base/views.py
+++ base/views.py
@@ -3,9 +3,15 @@
 
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from cms.models import Interview
 
 
 def index(request):
     """ view that renders a default home"""
-    return render_to_response('index.jade',
+
+    context = {
+        "interviews": Interview.objects.all()
+    }
+
+    return render_to_response('index.jade', context,
                               context_instance=RequestContext(request))
