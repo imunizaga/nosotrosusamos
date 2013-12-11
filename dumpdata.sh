5191382334da03ef044b12f9eab6291f2491492b
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2013-12-11 10:54:17 -0300

    New fixtures for cms app

--- dumpdata.sh
+++ dumpdata.sh
@@ -1,2 +1,3 @@
 python manage.py dumpdata auth > base/fixtures/initial_data.json --natural --indent=4
 python manage.py dumpdata users > users/fixtures/initial_data.json --natural --indent=4
+python manage.py dumpdata cms > cms/fixtures/initial_data.json --natural --indent=4
