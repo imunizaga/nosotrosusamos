476a46334724cf907f3a511a117ca75b97e8f53b
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2013-12-16 16:51:00 -0300

    New sidebar updater

--- base/static/js/site.js
+++ base/static/js/site.js
@@ -1,4 +1,5 @@
 /*global document: false */
+/*global location: false */
 /*global $: false */
 
 $(document).ready(function () {
@@ -6,4 +7,8 @@ $(document).ready(function () {
     $('[data-toggle=offcanvas]').click(function () {
         $('.row-offcanvas').toggleClass('active');
     });
+
+    $('a.list-group-item').removeClass('active');
+
+    $('a.list-group-item.' + location.pathname.replace(/\//g, '')).addClass('active');
 });
