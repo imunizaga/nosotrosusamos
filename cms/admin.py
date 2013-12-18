371aee4bbb3754450abbc9c7570a200ac61d9309
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2013-12-18 12:18:29 -0300

    New link for the admin interview list

--- cms/admin.py
+++ cms/admin.py
@@ -3,7 +3,17 @@ from django.contrib import admin
 
 
 class InterviewAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug']
+    list_display = ['title', 'slug', 'link']
+
+    def link(self, obj):
+        url = "/%s" % obj.slug
+
+        if obj.picture:
+            return "<a href='%s' target='_blank'>Link</a>" % url
+        return ""
+
+    link.allow_tags = True
+    link.short_description = 'Link'
 
 
 admin.site.register(Interview, InterviewAdmin)
